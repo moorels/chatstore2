e14f63d075d5e138326c1ca16ad45ad66f93991f
moorels <sales@micropls.com>
2023-07-01 20:02:53 +1000

    com1

---
+++ src/app/helpers/constants/book-data.ts
@@ -0,0 +1,109 @@
+export const bookData = `
+<url>
+<loc>https://www.micropls.com/.com/</loc>
+<desc>Micropls Computer Repair Services</desc>
+</url>
+
+we sell a large range of computer parts and accessories including but not limited to:
+
+CPU (Central Processing Unit)
+GPU (Graphics Processing Unit)
+RAM (Random Access Memory)
+Motherboard
+Hard Drive (HDD)
+Solid State Drive (SSD)
+Power Supply Unit (PSU)
+Computer Case
+Cooling Fans
+CPU Cooler
+Graphics Card
+Sound Card
+Network Card
+Optical Drive (CD/DVD/Blu-ray)
+Monitor
+Keyboard
+Mouse
+Speakers
+Webcam
+Microphone
+USB Flash Drive
+External Hard Drive
+Network Router
+Network Switch
+Network Cable
+Wireless Adapter
+Power Strip
+Surge Protector
+UPS (Uninterruptible Power Supply)
+Laptop Charger
+Laptop Battery
+Laptop Cooling Pad
+Laptop Stand
+Laptop Bag
+Monitor Stand
+Monitor Mount
+Docking Station
+Printer
+Scanner
+All-in-One Printer (Printer/Scanner/Copier)
+Ink Cartridges
+Toner Cartridges
+Mouse Pad
+Gaming Mouse
+Gaming Keyboard
+Gaming Headset
+Gaming Controller
+Gaming Chair
+External Sound Card
+Bluetooth Adapter
+Wi-Fi Range Extender
+Ethernet Cable
+HDMI Cable
+DisplayPort Cable
+VGA Cable
+DVI Cable
+Thunderbolt Cable
+USB Hub
+External DVD Drive
+External SSD
+NAS (Network Attached Storage)
+RAID Controller
+Server Rack
+Server Chassis
+Server Power Supply
+Server CPU
+Server RAM
+Server Hard Drives
+Server Network Card
+Server Cooling Fans
+Server Rails
+KVM Switch
+UPS Battery Backup
+Firewall Appliance
+Workstation Graphics Card
+Workstation Monitor
+Workstation Keyboard
+Workstation Mouse
+Workstation Speakers
+Workstation Desk
+Workstation Chair
+Workstation CPU
+Workstation RAM
+Workstation SSD
+Workstation Sound Card
+Workstation Network Card
+Workstation Cooling Solution
+External Graphics Enclosure
+Thunderbolt Dock
+Portable Hard Drive
+USB-C Docking Station
+USB-C Adapter
+HDMI Splitter
+USB Extension Cable
+Keyboard and Mouse Combo
+Laptop Privacy Filter
+Graphics Tablet
+UPS Management Card
+Laptop Lock
+Anti-Static Wrist Strap
+`
---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+
+    <main className='flex justify-left items-center text-center md:w-auto sm:w-auto bg-slate-700'>
+   <div>
+   <section className="text-white body-font bg-slate-700">
+  <div className="container px-5 py-12 mx-auto bg-slate-700">
+    
+    <div className="flex flex-wrap items-center text-center">
+      <div className="p-4 md:w-1/2 w-full ">
+        <div className=" bg-slate-600 p-8 rounded items-center text-center">
+        
+          <p className=' text-xl py-4'>Computer Repairs Services</p>
+          <p className="leading-relaxed mb-6 items-center text-center">
+Computer Repairs Services Store A.I Assistant
+Browse out example integration into a computer store below that handles sales and repairs of computers.
+Ask any questions you would generally ask a customer service staff member, to explore the depth of our 
+Automated assistant.
+          </p>
+         
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</section>
+
+   
+   </div>
+    </main>
+  )
+}
